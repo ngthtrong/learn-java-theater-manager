74e5e6858f690e80d2a5536539dabf42becf6164
Cao Tuong Hung <caotuonghung@gmail.com>
2024-10-25 11:43:49 +0700

    file test

--- src/main/java/ngthtrong/theatermanager/cc.java
+++ src/main/java/ngthtrong/theatermanager/cc.java
@@ -3,19 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ngthtrong.theatermanager;
-import  java.sql.Connection;
-import  java.sql.DatabaseMetaData;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author ASUS PC
  */
 public class cc {
+
     public static void main(String[] args) {
         Connect connect = new Connect();
-        connect.connect();
+        Connection s = connect.connect();
+        String sql = "select * from [user] where [user_id] = 1";
+        try {
+            PreparedStatement p = s.prepareStatement(sql);
+            ResultSet rs;
+            rs = p.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("username"));
+            }
+        }
+        catch (SQLException e) {
+            Logger.getLogger(cc.class.getName()).log(Level.SEVERE, null, e);
+        }
+
     }
 }
