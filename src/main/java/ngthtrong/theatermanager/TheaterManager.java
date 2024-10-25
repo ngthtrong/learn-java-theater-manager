55ddbcaf0a7700d366d2daeb016c763cee6a80eb
ngthtrong204 <ngthtrong204@gmail.com>
2024-10-25 09:48:15 +0700

    first commit

---
+++ src/main/java/ngthtrong/theatermanager/TheaterManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ngthtrong.theatermanager;
+
+/**
+ *
+ * @author jhiny
+ */
+public class TheaterManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
