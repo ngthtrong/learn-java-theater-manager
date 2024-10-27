d92df989d147202887bc33d6fe3402eff1c830de
ngthtrong204 <ngthtrong204@gmail.com>
2024-10-27 09:31:30 +0700

    fdjsk

--- src/main/java/ngthtrong/theatermanager/App.java
+++ src/main/java/ngthtrong/theatermanager/App.java
@@ -8,7 +8,7 @@ package ngthtrong.theatermanager;
  *
  * @author jhiny
  */
-public class TheaterManager {
+public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
