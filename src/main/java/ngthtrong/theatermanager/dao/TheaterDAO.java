fcdc9d8a0f9b85c3ddf39971b3a8d00ad8090b7b
ngthtrong204 <ngthtrong204@gmail.com>
2024-10-27 18:17:37 +0700

    init project

--- src/main/java/ngthtrong/theatermanager/dao/TheaterDAO.java
+++ src/main/java/ngthtrong/theatermanager/dao/TheaterDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package User;
+package ngthtrong.theatermanager.dao;
 
 /**
  *
  * @author jhiny
  */
-public class UserDAO {
+public class TheaterDAO {
     
 }
--- src/main/java/ngthtrong/theatermanager/data/Database.java
+++ src/main/java/ngthtrong/theatermanager/data/Database.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Connection;
+package ngthtrong.theatermanager.data;
 
 import  java.sql.Connection;
 import  java.sql.DatabaseMetaData;
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  *
  * @author ASUS PC
  */
-public class Connect {
+public class Database {
     public Connection connect(){
         String url = "jdbc:sqlserver://localhost:1433; databaseName= TheaterManager; user =root; password = root;" + "encrypt = true; trustServerCertificate = true";
         Connection sConn = null;
@@ -23,7 +23,7 @@ public class Connect {
             System.out.println("Ket noi thanh cong");
         }
         catch(SQLException e){
-            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, e);
             System.out.println("Ket noi that bai");
         }
         
