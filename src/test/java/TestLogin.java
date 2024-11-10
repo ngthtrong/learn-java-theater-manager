5bb1bf565e08f1a4e57609df05eefd6cd903afc7
ngthtrong204 <ngthtrong204@gmail.com>
2024-11-10 20:20:30 +0700

    done merge

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import ngthtrong.theatermanager.views.LoginForm;
+
+/**
+ *
+ * @author jhiny
+ */
+public class TestLogin {
+      public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+      }
+}
---
+++ src/test/java/TestRunUser.java
@@ -0,0 +1,10 @@
+
+
+import ngthtrong.theatermanager.controller.UserController;
+public class TestRunUser {
+    public static void main(String[] args) {
+        UserController uc = new UserController();
+        uc.userFormLoad();
+
+    }
+}
